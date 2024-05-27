4f128e0e6eeac40f1d788abac81032d552de2fb3
jpluta <jakub.pluta@zdt.io>
2024-05-27 17:16:48 +0200

    init

--- main.tf
+++ main.tf
@@ -1,1 +1,58 @@
-# Terraform code goes here
+resource "aws_db_parameter_group" "main" {
+  count = var.create_db_parameter_group ? 1 : 0
+
+  name   = var.parameter_group_name
+  family = "postgres16"
+
+  dynamic "parameter" {
+    for_each = var.parameters
+    content {
+      name         = parameter.value.name
+      value        = parameter.value.value
+      apply_method = lookup(parameter.value, "apply_method", null)
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage           = var.allocated_storage
+  identifier                  = var.instance_name
+  db_name                     = var.db_name
+  engine                      = "postgres"
+  engine_version              = var.engine_version
+  instance_class              = var.instance_class
+  manage_master_user_password = var.manage_master_user_password
+  username                    = var.username
+  password                    = var.password
+  parameter_group_name        = var.parameter_group_name
+
+  max_allocated_storage = var.max_allocated_storage
+  db_subnet_group_name  = var.db_subnet_group_name
+  multi_az              = var.multi_az
+
+  # replicate_source_db = var.replicate_source_db
+
+  storage_type = var.storage_type
+  # iops         = var.iops
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+  # enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  # must be grater than 0 if the db is used as a source for read replica
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  auto_minor_version_upgrade = false
+
+  # performance_insights_enabled          = true
+  # performance_insights_retention_period = 7
+  # monitoring_interval                   = 60
+
+  tags = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,131 @@
-# variable "variable_name" {
-#	  description = "variable description"
-#   type        = number
-#   default     = 1
-# }
+variable "allocated_storage" {
+  description = "A size of the DB storage."
+  type        = number
+  default     = 20
+}
 
-# variable "variable_password" {
-#	  description = "variable description"
-#   type        = string
-#   sensitive   = true
-#   default     = "abc"
-# }
+variable "max_allocated_storage" {
+  description = "The upper limit to which Amazon RDS can automatically scale the storage of the DB instance."
+  type        = number
+  default     = null
+}
+
+variable "multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ"
+  type        = bool
+  default     = false
+}
+
+variable "instance_name" {
+  description = "The database instance identifier."
+  type        = string
+}
+
+variable "db_name" {
+  description = "The database name."
+  type        = string
+  default     = "defaultdb"
+}
+
+variable "engine_version" {
+  description = "The engine version to use."
+  type        = string
+  default     = "16.3"
+}
+
+variable "availability_zone" {
+  description = "The availability zone of the instance"
+  type        = string
+  default     = null
+}
+
+variable "instance_class" {
+  description = "The instance type of the RDS instance."
+  type        = string
+  default     = "db.t4g.micro"
+}
+
+variable "password" {
+  description = "Password for the master DB user."
+  type        = string
+  default     = null
+}
+
+variable "manage_master_user_password" {
+  type    = bool
+  default = null
+}
+
+variable "username" {
+  description = "Username for the master DB user."
+  type        = string
+  default     = "dbadmin"
+}
+
+###
+
+variable "create_mode" {
+  description = "The creation mode. Can be used to restore or replicate existing servers."
+  type        = string
+  default     = "Default"
+}
+
+variable "creation_source_server_id" {
+  description = "For creation modes other then default the source server ID to use."
+  type        = string
+  default     = null
+}
+
+variable "restore_point_in_time" {
+  description = "When create_mode is PointInTimeRestore the point in time to restore from creation_source_server_id."
+  type        = string
+  default     = null
+}
+
+variable "create_db_parameter_group" {
+  description = "If true, a database parameter group is created."
+  type        = bool
+  default     = true
+}
+
+variable "parameter_group_name" {
+  description = "The name of the database parameter group."
+  type        = string
+  default     = null
+}
+
+variable "parameters" {
+  description = "A map of parameters included in the database parameter group."
+  default     = {}
+}
+
+variable "replicate_source_db" {
+  description = "Specifies that this a Replicate database."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags."
+  type        = map(string)
+  default     = {}
+}
+
+variable "db_subnet_group_name" {
+  description = "Name of DB subnet group."
+  type        = string
+  default     = null
+}
+
+
+variable "storage_type" {
+  description = "The database storage type."
+  type        = string
+  default     = "gp3"
+}
+
+variable "iops" {
+  description = "The database storage type."
+  type        = number
+  default     = 3000
+}
